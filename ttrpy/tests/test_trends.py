8da67241555c3535c5100c94a467a01317f9d746
joelowj <joel_ong@mymail.sutd.edu.sg>
2019-02-07 11:22:18 +0800

    [Configuration] added travis-ci config and requirements.txt containing a list of items to be installed using pip

--- ttrpy/tests/test_trends.py
+++ ttrpy/tests/test_trends.py
@@ -7,50 +7,54 @@ import unittest
 from ttrpy.trend.sma import sma
 from ttrpy.trend.ema import ema
 from ttrpy.trend.wma import wma
+from ttrpy.trend.macd import macd
 
 class TestTrendIndicators(unittest.TestCase):
 
     def setUp(self):
-        self.df = pd.read_csv("examples/weekly_MSFT.csv") \
+        self.wdf = pd.read_csv("examples/weekly_MSFT.csv") \
                     .sort_values(by="timestamp") \
                     .reset_index(drop=True)
+        self.ddf = pd.read_csv("examples/daily_MSFT.csv") \
+                     .sort_values(by="timestamp") \
+                     .reset_index(drop=True)
 
     def test_simple_moving_average(self):
-        self.df = sma(self.df, 'open', 'sma', 10).dropna() \
+        self.wdf = sma(self.wdf, 'open', 'sma', 10).dropna() \
                                                  .reset_index(drop=True)
-        self.assertEqual(len(self.df['sma']), 1092)
-        self.assertAlmostEqual(self.df['sma'][0], 124.8190, places=4)
-        self.assertAlmostEqual(self.df['sma'][1], 119.9380, places=4)
-        self.assertAlmostEqual(self.df['sma'][2], 115.5950, places=4)
-        self.assertAlmostEqual(self.df['sma'][1089], 104.5530, places=4)
-        self.assertAlmostEqual(self.df['sma'][1090], 104.3520, places=4)
-        self.assertAlmostEqual(self.df['sma'][1091], 104.1600, places=4)
+        self.assertEqual(len(self.wdf['sma']), 1092)
+        self.assertAlmostEqual(self.wdf['sma'][0], 124.8190, places=4)
+        self.assertAlmostEqual(self.wdf['sma'][1], 119.9380, places=4)
+        self.assertAlmostEqual(self.wdf['sma'][2], 115.5950, places=4)
+        self.assertAlmostEqual(self.wdf['sma'][1089], 104.5530, places=4)
+        self.assertAlmostEqual(self.wdf['sma'][1090], 104.3520, places=4)
+        self.assertAlmostEqual(self.wdf['sma'][1091], 104.1600, places=4)
 
     def test_exponential_moving_average(self):
-        self.df = ema(self.df, 'open', 'ema', 10).dropna() \
+        self.wdf = ema(self.wdf, 'open', 'ema', 10).dropna() \
                                                  .reset_index(drop=True)
-        self.assertEqual(len(self.df['ema']), 1092)
-        self.assertAlmostEqual(self.df['ema'][0], 124.8190, places=4)
-        self.assertAlmostEqual(self.df['ema'][1], 117.1137, places=4)
-        self.assertAlmostEqual(self.df['ema'][2], 110.5821, places=4)
-        self.assertAlmostEqual(self.df['ema'][1089], 104.5003, places=4)
-        self.assertAlmostEqual(self.df['ema'][1090], 104.8202, places=4)
-        self.assertAlmostEqual(self.df['ema'][1091], 104.4656, places=4)
+        self.assertEqual(len(self.wdf['ema']), 1092)
+        self.assertAlmostEqual(self.wdf['ema'][0], 124.8190, places=4)
+        self.assertAlmostEqual(self.wdf['ema'][1], 117.1137, places=4)
+        self.assertAlmostEqual(self.wdf['ema'][2], 110.5821, places=4)
+        self.assertAlmostEqual(self.wdf['ema'][1089], 104.5003, places=4)
+        self.assertAlmostEqual(self.wdf['ema'][1090], 104.8202, places=4)
+        self.assertAlmostEqual(self.wdf['ema'][1091], 104.4656, places=4)
 
     def test_weighted_moving_average(self):
-        self.df = wma(self.df, 'open', 'wma', 10).dropna() \
+        self.wdf = wma(self.wdf, 'open', 'wma', 10).dropna() \
                                                  .reset_index(drop=True)
-        self.assertEqual(len(self.df['wma']), 1092)
-        self.assertAlmostEqual(self.df['wma'][0], 116.5182, places=4)
-        self.assertAlmostEqual(self.df['wma'][1], 108.8129, places=4)
-        self.assertAlmostEqual(self.df['wma'][2], 101.7678, places=4)
-        self.assertAlmostEqual(self.df['wma'][1089], 103.5624, places=4)
-        self.assertAlmostEqual(self.df['wma'][1090], 103.8727, places=4)
-        self.assertAlmostEqual(self.df['wma'][1091], 103.6033, places=4)
-
+        self.assertEqual(len(self.wdf['wma']), 1092)
+        self.assertAlmostEqual(self.wdf['wma'][0], 116.5182, places=4)
+        self.assertAlmostEqual(self.wdf['wma'][1], 108.8129, places=4)
+        self.assertAlmostEqual(self.wdf['wma'][2], 101.7678, places=4)
+        self.assertAlmostEqual(self.wdf['wma'][1089], 103.5624, places=4)
+        self.assertAlmostEqual(self.wdf['wma'][1090], 103.8727, places=4)
+        self.assertAlmostEqual(self.wdf['wma'][1091], 103.6033, places=4)
 
     def tearDown(self):
-        self.df = None
+        self.ddf = None
+        self.wdf = None
 
 if __name__ == '__main__':
     unittest.main()
