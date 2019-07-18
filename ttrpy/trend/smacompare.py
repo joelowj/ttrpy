74002f5ae2b95a00c15bc860bcd339d257caf11c
actlienholding <actlien@actlien.com>
2019-07-18 14:19:07 -0400

    Update smacompare.py

--- ttrpy/trend/smacompare.py
+++ ttrpy/trend/smacompare.py
@@ -13,4 +13,6 @@ def smacompare3(df,price,sma1,sma2,sma3,n1,n2,n3):
 	df = sma(df,price,sma2,n2)
 	df = sma(df,price,sma3,n3)
 	df[sma1+'/'+sma2] = df[sma1]/df[sma2]
+	df[sma2+'/'+sma3] = df[sma2]/df[sma3]
+	df[sma1+'/'+sma3] = df[sma1]/df[sma3]
 	return df     
