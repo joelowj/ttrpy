6c61a76f9e77c4048179d1e70b8b9d758c95eac1
actlienholding <actlien@actlien.com>
2019-07-18 14:14:54 -0400

    Create smacompare.py

---
+++ ttrpy/trend/smacompare.py
@@ -0,0 +1,18 @@
+from ttrpy.trend.sma import sma  
+import pandas as pd 
+
+
+#compares 2 smas
+def smacompare(df,price,sma1,sma2,n1,n2):
+	df = sma(df,price,sma1,n1)  
+	df = sma(df,price,sma2,n2)
+	df[sma1+'/'+sma2] = df[sma1]/df[sma2]
+	return df     
+
+#compares 3 smas
+def smacompare3(df,price,sma1,sma2,sma3,n1,n2,n3):
+	df = sma(df,price,sma1,n1)  
+	df = sma(df,price,sma2,n2)
+	df = sma(df,price,sma3,n3)
+	df[sma1+'/'+sma2] = df[sma1]/df[sma2]
+	return df     
